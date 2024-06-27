412eda516c782256ae37cffe406d5f8204a8494a
HrishikeshChaudhari24 <chaudharihrishikesh30@gmail.com>
2024-06-27 18:11:56 +0530

    Update install_python.sh

--- install_python.sh
+++ install_python.sh
@@ -2,7 +2,7 @@
 
 # Update package list and install Python and pip
 apt-get update
-apt-get install -y python3 python3-pip
+apt-get install -y python3.9 python3-pip
 
 # Install Python dependencies
 pip3 install -r requirement.txt
